39527f1884045645b71d86970f876573daf6698f
Will Griffin <will@grifffin.uk>
2023-12-14 11:37:42 +0000

    SQL statements for testing db functionality

---
+++ mvc/database/db.sql
@@ -0,0 +1,6 @@
+USE horizonRestaurant;
+
+-- INSERT for wills test account
+INSERT INTO employee (employee_id, employee_name, employee_account_type, employee_password) VALUES
+(1234, "Will Griffin","Standard","123");
+
