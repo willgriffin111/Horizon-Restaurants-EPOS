12bc3c6d7a8c18e8bff5f7cdad8a9e26b982f6ca
Will Griffin <will@grifffin.uk>
2024-03-18 11:18:04 +0000

    cleanup

--- mainTest.py
+++
@@ -1,8 +0,0 @@
-# from Views.main import Add
-
-# def TestAdd():
-#         assert Add(2,3) == 5
-#         print("Add Function works correctly")
-
-# if __name__ == '__main__':
-#         TestAdd()
